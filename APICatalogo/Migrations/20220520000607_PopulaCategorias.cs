7ba83b60b14a35ad02140a35adfc19d681abee3e
Italo Teles <italop.teles@gmail.com>
2022-05-19 21:22:34 -0300

    Mapeamento e Criação do banco de dados com Migrations

---
+++ APICatalogo/Migrations/20220520000607_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Bebidas','bebidas.jpg')");
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Lanches','lanches.jpg')");
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Sobremesas','sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from Categorias");
+        }
+    }
+}
--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,9 +1,23 @@
-﻿namespace APICatalogo.Models;  
+﻿  using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models;  
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
     public int CategoriaId { get; set; }
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [MaxLength(300)]
     public string? ImagemURL { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 }
 
